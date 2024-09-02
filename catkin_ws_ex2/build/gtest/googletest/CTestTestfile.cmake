d14e06c1523bbc4ba4b3eec4cb069678196047b6
rakhat123 <haise.sasaki@list.ru>
2024-09-03 00:31:46 +0500

    Adding initial files of catkin_ws_ex2

---
+++ catkin_ws_ex2/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/rakhat123/lab1_git/catkin_ws_ex2/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws_ex2/build/turtlebot_controller/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/rakhat123/lab1_git/catkin_ws_ex2/devel/lib/turtlebot_controller/listener"
+  "/home/rakhat123/lab1_git/catkin_ws_ex2/devel/lib/turtlebot_controller/listener.pdb"
+  "CMakeFiles/listener.dir/src/subscriber.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
